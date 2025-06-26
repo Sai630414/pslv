425fd6974ecf00665e6ca0f6ab5a92b95df4adec
Sai630414 <saikondareddypala@gmail.com>
2025-06-26 22:19:04 +0530

    fifth

--- src/app/home/add/add.ts
+++ src/app/home/add/add.ts
@@ -1,12 +1,19 @@
+import { CommonModule, CurrencyPipe, DatePipe } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-add',
-  imports: [],
+  imports: [CurrencyPipe,CommonModule,DatePipe],
   templateUrl: './add.html',
   styleUrl: './add.css'
 })
 export class Add {
+  amount=2999;
+  amount1=4999;
+  amount2=1499;
+  dat = new Date(2025, 6, 31);
+  
+  
    ngAfterViewInit() {
     const elements = document.querySelectorAll('.scroll-animate');
 
