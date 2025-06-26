8fbc14bd780d2e503d3cf89e306a255a4e7ea9a3
Sai630414 <saikondareddypala@gmail.com>
2025-06-26 12:34:33 +0530

    fourth

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { Login } from './login/login';
 import { Home } from './home/home';
+import { Signup } from './login/signup/signup';
+
 
 
 
@@ -8,6 +10,8 @@ export const routes: Routes = [
     {
         path:"login",
         component:Login
+    },{
+        path:"signup",component:Signup
     },
     {
         path:'',component:Home,
